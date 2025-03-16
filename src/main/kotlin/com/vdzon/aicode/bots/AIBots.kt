40bc4b73b7e67264060acdae30bd33b99732ef82
Robbert vd Zon <robbert.vanderzon@edsn.nl>
2025-03-16 09:25:57 +0100

    initial version

--- src/main/kotlin/com/vdzon/aicode/bots/AIBots.kt
+++ src/main/kotlin/com/vdzon/aicode/bots/AIBots.kt
@@ -3,13 +3,15 @@ package com.vdzon.aicode.bots
 import com.vdzon.aicode.bots.checkstory.CheckStoryBot
 import com.vdzon.aicode.bots.codegenerator.CodeGeneratorBot
 import com.vdzon.aicode.bots.codereview.CodeReviewBot
+import com.vdzon.aicode.bots.createbranch.CreateBranchBot
 
 object AIBots {
     fun getAllBots(): List<AIBot> {
         return listOf(
             CheckStoryBot(),
             CodeGeneratorBot(),
-            CodeReviewBot()
+            CodeReviewBot(),
+            CreateBranchBot()
         )
     }
 }
